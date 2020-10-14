2d6931fb6d19d9c38cfb8c4fe586312631c88e05
Makdoom Shaikh <makshaikh99@gmail.com>
2020-10-14 14:32:03 +0530

    Validating request body

--- model/ValidationSchema.js
+++ model/ValidationSchema.js
@@ -1,1 +1,47 @@
 const joi = require("joi");
+
+module.exports = {
+  Schemas: {
+    // Register Schema
+    register: joi.object({
+      name: joi.string().required().max(15),
+      email: joi.string().email().required(),
+      password: joi.string().min(6).max(20).required(),
+      confirmPassword: joi.string().required().valid(joi.ref("password")),
+    }),
+
+    // Login Schema
+    login: joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().min(6).max(20).required(),
+    }),
+  },
+
+  Validation: {
+    //  Registration Validator
+    register: (schema) => {
+      return (req, res, next) => {
+        const result = schema.validate(req.body);
+
+        // if there's any error
+        if (result.error) {
+          if (result.error.details[0].path == "confirmPassword")
+            return res.status(400).json({ error: "Password do not match " });
+
+          return res
+            .status(400)
+            .json({ error: result.error.details[0].message });
+          //   return res.status(400).json(result.error);
+        }
+
+        if (!req.value) {
+          req.value = {};
+        }
+
+        req.value["body"] = result.value;
+
+        next();
+      };
+    },
+  },
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
+const { Schemas, Validation } = require("../model/ValidationSchema");
 
 // Register Route
-router.post("/register", (req, res) => {
-  res.send("Register");
+router.post("/register", Validation.register(Schemas.register), (req, res) => {
+  const { name, email, password } = req.value.body;
+  //   res.status(200).json({ user: req.value.body });
+  console.log(req.value.body);
 });
 
 // Login Route
