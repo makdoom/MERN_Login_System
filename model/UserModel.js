f8c31be9c55f29081ae7b9a9b6a5538b9ca3597d
Makdoom Shaikh <makshaikh99@gmail.com>
2020-10-15 14:13:03 +0530

    Creating a model

---
+++ model/UserModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// User Schema
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
