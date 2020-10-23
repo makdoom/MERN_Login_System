b9b91d265271cb8c488621bccdb9b9dca8ff3e68
Makdoom Shaikh <makshaikh99@gmail.com>
2020-10-23 16:44:54 +0530

    Preparing for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const path = require("path");
 require("dotenv").config();
 
 // Initialization
@@ -29,6 +30,13 @@ app.get("/", (req, res) => {
 
 app.use("/auth/users", require("./routes/authRoutes"));
 
+// Serve static file in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // Start server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running at ${port}`));
